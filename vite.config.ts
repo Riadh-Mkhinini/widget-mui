e3fa54f3a8d2c2c175dcb28af1bc84ee0c4d714a
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-14 13:46:48 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       fileName: () => `load-widget.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      // external: ["react", "react-dom"],
     },
   },
 });
