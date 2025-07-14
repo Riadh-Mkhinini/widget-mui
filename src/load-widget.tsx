44a03b360e039a778a158c866be601afb87e1ebc
Riadh Mkhinini <riadh.m@trvlcode.com>
2025-07-14 13:48:53 +0100

    testss

--- src/load-widget.tsx
+++ src/load-widget.tsx
@@ -1,3 +1,5 @@
+(window as any).process = { env: { NODE_ENV: "production" } };
+
 import ReactDOM from "react-dom/client";
 import Widget from "./widget/widget";
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
   build: {
     lib: {
       entry: "src/load-widget.tsx",
       formats: ["es"],
       fileName: () => `load-widget.js`,
     },
-    rollupOptions: {
-      // external: ["react", "react-dom"],
-    },
+    // rollupOptions: {
+    // external: ["react", "react-dom"],
+    // },
   },
 });
